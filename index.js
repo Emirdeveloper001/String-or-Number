1334ac63de5d35cf767591089dfa0193755c3bd5
unknown <emirdeveloper2001@gmail.com>
2021-09-22 17:15:32 +0330

    example of middle number in quote added and explained

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ var b = 2 + 7 + 5;
 document.getElementById('example2').innerHTML = b
 var c = "2" + 7 + 5;
 document.getElementById('example3').innerHTML = c
+var d = 1 + 2 + "7" + 5 + 9;
+document.getElementById('example4').innerHTML = d
