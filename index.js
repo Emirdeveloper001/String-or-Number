29bddf9c800589afd6f469d78d7f8f55911e0148
unknown <emirdeveloper2001@gmail.com>
2021-09-22 17:07:39 +0330

    the example of first number in string added and explained

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ var a = "2" + "7" + "5";
 document.getElementById('example1').innerHTML = a
 var b = 2 + 7 + 5;
 document.getElementById('example2').innerHTML = b
+var c = "2" + 7 + 5;
+document.getElementById('example3').innerHTML = c
